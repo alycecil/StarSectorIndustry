9a00a0b425d290ba79305a0b5d411dcea49058bf
The Cecil <alycecil@gmail.com>
2021-09-11 02:13:08 -0400

    AI Builds the Things in demi-random order

--- src/main/java/com/github/alycecil/econ/ai/colony/MarketCommander.java
+++ src/main/java/com/github/alycecil/econ/ai/colony/MarketCommander.java
@@ -3,18 +3,29 @@ package com.github.alycecil.econ.ai.colony;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.econ.Industry;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
+import com.fs.starfarer.api.campaign.econ.MarketConditionAPI;
 import com.fs.starfarer.api.campaign.listeners.EconomyTickListener;
+import com.fs.starfarer.api.impl.campaign.econ.ResourceDepositsCondition;
 import com.fs.starfarer.api.impl.campaign.econ.impl.BaseIndustry;
 import com.fs.starfarer.api.impl.campaign.econ.impl.ConstructionQueue;
 import com.fs.starfarer.api.impl.campaign.ids.Commodities;
 import com.fs.starfarer.api.impl.campaign.ids.Industries;
+import com.fs.starfarer.api.util.Misc;
+import com.fs.starfarer.api.util.WeightedRandomPicker;
+import org.apache.log4j.Logger;
 
 import java.util.List;
+import java.util.Random;
 
+import static com.fs.starfarer.api.impl.campaign.econ.impl.Farming.AQUA_PLANETS;
 import static com.github.alycecil.econ.util.AliceIndustries.*;
 
 public class MarketCommander implements EconomyTickListener {
+    private static final int COST = 100;
+
     MarketAPI market;
+    private volatile static Random random = new Random();
+    private volatile static Logger logger = Global.getLogger(MarketCommander.class);
 
     public MarketCommander(MarketAPI market) {
         this.market = market;
@@ -28,30 +39,32 @@ public class MarketCommander implements EconomyTickListener {
     @Override
     public void reportEconomyMonthEnd() {
         if (market == null) {
-            Global.getLogger(this.getClass()).warn("Market Null");
+            logger.warn("Market Null");
             return;
-        } else if (market.isPlayerOwned()
-                || market.getFaction() == null
+        } else if (market.getFaction() == null
         ) {
-            Global.getLogger(this.getClass()).info("Market Unfit, " + market.getName());
+            logger.info("Market Unfit, " + market.getName());
             return;
+        } else if (market.isPlayerOwned()) {
+            if (random.nextInt(10) > 1) { //rarely do for player stuff
+                logger.debug("Player market sleeping " + market.getName());
+                return;
+            }
         }
 
         if (market.getConstructionQueue() == null) {
-            Global.getLogger(this.getClass()).warn("Market unable to build with queue, " + market.getName());
+            logger.warn("Market unable to build with queue, " + market.getName());
             return;
         }
 
         boolean building = isBuilding();
 
         if (!building) {
-            //todo debug
-            Global.getLogger(this.getClass()).info("Market " + market.getName() + " looking for construction.");
+            logger.debug("Market " + market.getName() + " looking for construction.");
 
             prepareConstruction();
         } else {
-            //todo debug
-            Global.getLogger(this.getClass()).info("Market " + market.getName() + " building.");
+            logger.debug("Market " + market.getName() + " building.");
         }
     }
 
@@ -59,8 +72,7 @@ public class MarketCommander implements EconomyTickListener {
         boolean building = false;
         for (Industry industry : market.getIndustries()) {
             if (industry.isBuilding()) {
-                //todo debug
-                Global.getLogger(this.getClass()).info("Market " + market.getName() + " building a " + industry.getCurrentName() + ".");
+                logger.debug("Market " + market.getName() + " building a " + industry.getCurrentName() + ".");
 
                 building = true;
                 break;
@@ -70,124 +82,224 @@ public class MarketCommander implements EconomyTickListener {
     }
 
     protected void prepareConstruction() {
-        boolean build = false;
+        boolean build;
 
         ConstructionQueue constructionQueue = market.getConstructionQueue();
         if (constructionQueue == null) return;
         List<ConstructionQueue.ConstructionQueueItem> items = constructionQueue.getItems();
         if (items == null) return;
         if (items.isEmpty()) {
-            //todo debug
-            Global.getLogger(this.getClass()).info("Market " + market.getName() + " queueing new work.");
+            logger.debug("Market " + market.getName() + " queueing new work.");
             build = buildQueue(constructionQueue);
         } else {
-            Global.getLogger(this.getClass()).info("Market " + market.getName() + " already queued work.");
+            logger.debug("Market " + market.getName() + " already queued work.");
 
             build = true;
         }
 
         if (build) {
+            if (market.getSize() < random.nextInt(10)) {
+                //todo debug
+                logger.info("Market " + market.getName() + " rolled not ready.");
+                return;
+            }
+
             //todo debug
-            Global.getLogger(this.getClass()).info("Market " + market.getName() + " starting building.");
+            logger.info("Market " + market.getName() + " starting building.");
 
             BaseIndustry.buildNextInQueue(market);
+        }else{
+            for (Industry industry : market.getIndustries()) {
+                if(industry.canUpgrade() && !industry.isUpgrading()){
+                    industry.startUpgrading();
+                    break;
+                }
+            }
         }
     }
 
-    //TODO remove tech-debt.
-    boolean did; //dont overqueue, lazy and bad way should be better "market plans" loop but this is POC land and is just a simple "ai"
 
-    protected synchronized /*jic see TODO*/ boolean buildQueue(ConstructionQueue constructionQueue) {
-        did = false;
+    protected boolean buildQueue(ConstructionQueue constructionQueue) {
         //dear lord let has industry by on a hashtable and not an O(n) list scan.
         //if not well at least only end of month sucks.
 
-        float modifiedValue = market.getStability().getModifiedValue();
+        float stability = market.getStability().getModifiedValue();
 
         boolean cruel = market.getFaction().isIllegal(Commodities.HAND_WEAPONS)
                 && !market.getFaction().isIllegal(Commodities.ORGANS);
 
-        if (modifiedValue < 5) {
-            if (!market.hasIndustry(PoliceState)) {
-                constructionQueue.addToEnd(PoliceState, 1000);
-            } else if (cruel && !market.hasIndustry(AuthoritarianRegime) && modifiedValue < 3 && market.getSize() > 4) {
-                //need a large enough population for tribalism to falter to the point where otherism starts and survival is not a primary concern.
-                constructionQueue.addToEnd(AuthoritarianRegime, 1000);
-            }
-        }
+        if (doStability(constructionQueue, stability, cruel)) return true;
 
         boolean farmers = market.hasIndustry(Industries.FARMING);
         boolean waterFarmers = market.hasIndustry(Industries.AQUACULTURE);
+        boolean miners = market.hasIndustry(Industries.MINING);
 
-        //TODO Make Picker.
+        int num = Misc.getNumIndustries(market);
+        int max = Misc.getMaxIndustries(market);
+
+        if (addMissingIndustriesPriority(constructionQueue, farmers, waterFarmers, miners, num, max)) return true;
+
+
+        return doPicker(constructionQueue, stability, cruel, farmers, waterFarmers, miners, num, max);
+
+    }
+
+    private boolean hasConditions(String desiredIndustry) {
+        for (MarketConditionAPI mc : market.getConditions()) {
+            String commodity = ResourceDepositsCondition.COMMODITY.get(mc.getId());
+            if (commodity != null) {
+                String industry = ResourceDepositsCondition.INDUSTRY.get(commodity);
+                if (desiredIndustry.equals(industry)) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean doStability(ConstructionQueue constructionQueue, float stability, boolean cruel) {
+        if (stability < 5) {
+            if (!market.hasIndustry(PoliceState)) {
+                constructionQueue.addToEnd(PoliceState, COST);
+                return true;
+            } else if (cruel && !market.hasIndustry(AuthoritarianRegime) && market.getSize() > 4) {
+                //need a large enough population for tribalism to falter to the point where otherism starts and survival is not a primary concern.
+                constructionQueue.addToEnd(AuthoritarianRegime, COST);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean doPicker(ConstructionQueue constructionQueue, float stability, boolean cruel, boolean farmers, boolean waterFarmers, boolean miners, int num, int max) {
+        WeightedRandomPicker<String> picker = new WeightedRandomPicker<>(random);
+        boolean hasSpace = num >= max;
+
         switch (market.getSize()) {
             case 10:
-                queueIfNotPresent(constructionQueue, PopulationWealthy);
-                if (farmers) {
-                    queueIfNotPresent(constructionQueue, AutomatedFarming);
-                }
-                if (cruel && modifiedValue > 6) {
-                    queueIfNotPresent(constructionQueue, ChopShop);
+                if (hasSpace) {
+                    queueIfNotPresent(picker, PopulationWealthy, 10);
+
+                    if (farmers) {
+                        queueIfNotPresent(picker, AutomatedFarming);
+                    }
+                    if (cruel && stability > 6) {
+                        queueIfNotPresent(picker, ChopShop);
+                    }
                 }
             case 9:
-                queueIfNotPresent(constructionQueue, SpaceElevator);
-                queueIfNotPresent(constructionQueue, GatesCustomsNavy);
-                if (farmers) {
-                    queueIfNotPresent(constructionQueue, IndustrialFarming);
+                queueIfNotPresent(picker, SpaceElevator);
+                queueIfNotPresent(picker, GatesCustomsNavy);
+                if (farmers && hasSpace) {
+                    queueIfNotPresent(picker, IndustrialFarming);
                 }
             case 8:
                 if (market.hasIndustry(Industries.HEAVYINDUSTRY)) {
-                    queueIfNotPresent(constructionQueue, IndustrialDefenseForce);
-                }else{
-                    queueIfNotPresent(constructionQueue, SelfDefenseForce);
+                    queueIfNotPresent(picker, IndustrialDefenseForce);
+                } else {
+                    queueIfNotPresent(picker, SelfDefenseForce);
                 }
-                queueIfNotPresent(constructionQueue, CivilianInfrastructureLuxury);
-                if (farmers) {
-                    queueIfNotPresent(constructionQueue, StateFarms);
+                queueIfNotPresent(picker, CivilianInfrastructureLuxury);
+                if (farmers && hasSpace) {
+                    queueIfNotPresent(picker, StateFarms);
                 }
             case 7:
-                queueIfNotPresent(constructionQueue, CivilianInfrastructureCommon);
 
-                if (!farmers && !waterFarmers && market.hasIndustry(Industries.FUELPROD)) {
-                    queueIfNotPresent(constructionQueue, BulkFuelProduction);
+                queueIfNotPresent(picker, CivilianInfrastructureCommon);
+
+                if (hasSpace) {
+                    if (farmers || waterFarmers && market.hasIndustry(CivilianInfrastructureLuxury)) {
+                        //has a lot of nice things already, even if its shrank one.
+                        queueIfNotPresent(picker, PopulationWealthy, 5);
+                    }
+
+                    if (!farmers && !waterFarmers && market.hasIndustry(Industries.FUELPROD)) {
+                        queueIfNotPresent(picker, BulkFuelProduction);
+                    }
                 }
             case 6:
-                queueIfNotPresent(constructionQueue, TransportationInfrastructure);
+                queueIfNotPresent(picker, TransportationInfrastructure);
 
                 if (market.hasIndustry(Industries.ORBITALWORKS)) {
-                    queueIfNotPresent(constructionQueue, IndustrialDefenseForce);
+                    queueIfNotPresent(picker, IndustrialDefenseForce);
                 }
             case 5:
                 if (waterFarmers) {
-                    queueIfNotPresent(constructionQueue, AquacultureExtensions);
-                } else if (!farmers && market.hasIndustry(Industries.MINING)) {
-                    queueIfNotPresent(constructionQueue, TraceMining);
+                    queueIfNotPresent(picker, AquacultureExtensions);
+                } else if (!farmers && miners && hasSpace) {
+                    queueIfNotPresent(picker, TraceMining);
                 }
             case 4:
                 if (market.hasIndustry(Industries.HIGHCOMMAND)) {
                     if (cruel) {
-                        queueIfNotPresent(constructionQueue, PoliceState);
-                    } else {
-                        queueIfNotPresent(constructionQueue, GatesCustomsNavy);
+                        queueIfNotPresent(picker, PoliceState);
                     }
-                    queueIfNotPresent(constructionQueue, Industries.FUELPROD);
+
+                    queueIfNotPresent(picker, GatesCustomsNavy);
+
+                    if (hasSpace) {
+                        queueIfNotPresent(picker, Industries.FUELPROD);
+                    }
+                }
+                if (hasSpace) {
+                    if (miners && (market.hasIndustry(Industries.ORBITALWORKS) || market.hasIndustry(Industries.HEAVYINDUSTRY))) {
+                        queueIfNotPresent(picker, Industries.REFINING);
+                    }
                 }
             default:
                 if (farmers) {
-                    queueIfNotPresent(constructionQueue, RuralFarmCoOp);
+                    queueIfNotPresent(picker, RuralFarmCoOp);
                 }
-
+                if (hasSpace) {
+                    addMissingIndustriesPicker(picker);
+                }
         }
-        return did;
+
+        String result = picker.pick();
+        if (result == null) {
+            return false;
+        }
+        constructionQueue.addToEnd(result, COST);
+        return true;
     }
 
-    private void queueIfNotPresent(ConstructionQueue constructionQueue, String id) {
-        if (!did && !market.hasIndustry(id)) {
-            //todo debug
-            Global.getLogger(this.getClass()).info("Market " + market.getName() + " should build " + id + " next.");
+    private void addMissingIndustriesPicker(WeightedRandomPicker<String> picker) {
+        if (hasConditions(Industries.TECHMINING)) {
+            queueIfNotPresent(picker, Industries.TECHMINING);
+        }
+    }
 
-            constructionQueue.addToEnd(id, 1000);
-            did = true;
+    private boolean addMissingIndustriesPriority(ConstructionQueue constructionQueue, boolean farmers, boolean waterFarmers, boolean miners, int num, int max) {
+
+        if (num >= max) return false;
+
+        if (!farmers && !waterFarmers && hasConditions(Industries.FARMING)) {
+            boolean canAquaculture = market.getPlanetEntity() != null &&
+                    AQUA_PLANETS.contains(market.getPlanetEntity().getTypeId());
+
+            if (canAquaculture) {
+                constructionQueue.addToEnd(Industries.AQUACULTURE, COST);
+            } else {
+                constructionQueue.addToEnd(Industries.FARMING, COST);
+            }
+
+            return true;
+
+        }
+        if (!miners && hasConditions(Industries.MINING)) {
+            constructionQueue.addToEnd(Industries.MINING, COST);
+            return true;
+        }
+        return false;
+    }
+
+    private void queueIfNotPresent(WeightedRandomPicker<String> picker, String id) {
+        queueIfNotPresent(picker, id, 100);
+    }
+
+    private void queueIfNotPresent(WeightedRandomPicker<String> picker, String id, int weight) {
+        if (!market.hasIndustry(id)) {
+            logger.debug("Market " + market.getName() + " should build " + id + " next.");
+
+            picker.add(id, weight);
         }
     }
 }
